4ebcb1beda56c40ca88c3e513e364e212cd9f1c9
Andrew Ponomarev <spargo32@gmail.com>
2022-05-21 17:47:04 +0300

    Mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      headings: ["Fraunces", ...defaultTheme.fontFamily.serif],
+      content: ["Barlow", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      backgroundImage: {
+        "image-header": "url('../images/mobile/image-header.jpg')",
+        "image-graphic-design":
+          "url('../images/mobile/image-graphic-design.jpg')",
+        "image-photography": "url('../images/mobile/image-photography.jpg')",
+      },
+      colors: {
+        beige: "hsl(26, 100%, 99%)",
+        yellow: "hsl(51, 100%, 49%)",
+        red: "hsl(7, 99%, 70%)",
+        green: {
+          DEFAULT: "hsl(167, 40%, 24%)",
+          light: "hsl(167, 44%, 70%)",
+          dark: "hsl(168, 34%, 41%)",
+        },
+        blue: "hsl(198, 62%, 26%)",
+        gray: {
+          darkest: "hsl(212, 27%, 19%)",
+          darker: "hsl(213, 9%, 39%)",
+        },
+      },
+      letterSpacing: {
+        widest: "0.2rem",
+      },
+    },
   },
   plugins: [],
 };
